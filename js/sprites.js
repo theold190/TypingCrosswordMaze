5c66b6e14cb49a7b8e369a22d325f0715e733417
TheOld <the.old@gmail.com>
2012-07-31 22:41:07 +0200

    Adding a treasure sprite

--- js/sprites.js
+++ js/sprites.js
@@ -4,6 +4,9 @@ Crafty.sprite(80, "res/hero_80.png", {
 Crafty.sprite(80, "res/hole_80.png", {
     sprite_portal: [0, 0]
 });
+Crafty.sprite(80, "res/treasure_80.png", {
+    sprite_finish: [0, 0]
+});
 Crafty.sprite(400, "res/background.png", {
     sprite_background: [0, 0]
 });
